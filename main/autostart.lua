eb637ebf4d7986c4e2ee0a919079bd2f795a4039
Albert Tang <tangalbert919@yahoo.com>
2021-10-07 12:00:28 -0500

    Moduarize everything
    Entirely based on this:
    https://gitlab.com/epsi-rns/dotfiles/-/tree/master/awesome

---
+++ main/autostart.lua
@@ -0,0 +1,12 @@
+-- Standard awesome library
+local awful = require("awful")
+
+awful.spawn.with_shell(
+    'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;' ..
+    'xrdb -merge <<< "awesome.started:true";' ..
+    -- list each of your autostart commands, followed by ; inside single quotes, followed by ..
+    --'dex --environment Awesome --autostart --search-paths "$XDG_CONFIG_DIRS/autostart:$XDG_CONFIG_HOME/autostart"' -- https://github.com/jceb/dex
+    'picom -b;' ..
+    'conky --pause=5;' ..
+    'discord'
+)
---
+++ main/menu.lua
@@ -0,0 +1,79 @@
+-- Standard awesome library
+local awful = require("awful")
+local hotkeys_popup = require("awful.hotkeys_popup").widget
+-- Theme handling library
+local beautiful = require("beautiful") -- for awesome.icon
+
+local M = {}  -- menu
+local _M = {} -- module
+
+-- reading
+-- https://awesomewm.org/apidoc/popups%20and%20bars/awful.menu.html
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+-- This is used later as the default terminal and editor to run.
+-- local terminal = "xfce4-terminal"
+local terminal = RC.vars.terminal
+
+-- Variable definitions
+-- This is used later as the default terminal and editor to run.
+local editor = os.getenv("EDITOR") or "nano"
+local editor_cmd = terminal .. " -e " .. editor
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+M.awesome = {
+  { "Hotkeys", function() 
+      hotkeys_popup.show_help(nil, awful.screen.focused()) 
+    end },
+  { "Manual", terminal .. " -e man awesome" },
+  { "Edit config", editor_cmd .. " " .. awesome.conffile },
+  { "Restart", awesome.restart },
+  { "Quit", function() awesome.quit() end }
+}
+
+M.applications = {
+  { "Discord", "discord" },
+--  { "firefox", "firefox", awful.util.getdir("config") .. "/firefox.png" },
+  { "Google Chrome", "google-chrome-stable" },
+--  { "&firefox", "firefox" },
+  { "Thunderbird", "thunderbird" },
+  { "Telegram", "telegram-desktop" },
+  { "LibreOffice", "libreoffice" },
+  { "transmission", "transmission-gtk" },
+  { "screenshooter", "xfce4-screenshooter" },
+  { "Volume Control", "pavucontrol" },
+  { "Visual Studio Code", "code"}
+}
+
+M.power = {
+  { "Shutdown", "systemctl poweroff" },
+  { "Suspend", "systemctl suspend" },
+  { "Reboot", "systemctl reboot" }
+}
+
+--M.network_main = {
+--  { "wicd-curses", "wicd-curses" },
+--  { "wicd-gtk", "wicd-gtk" }
+--}
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+function _M.get()
+
+  -- Main Menu
+  local menu_items = {
+    { "awesome", M.awesome, beautiful.awesome_subicon },
+    { "Open terminal", terminal },
+--    { "network", M.network_main },
+    { "Applications", M.applications },
+    { "Power", M.power }
+  }
+
+  return menu_items
+end
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+return setmetatable({}, { __call = function(_, ...) return _M.get(...) end })
