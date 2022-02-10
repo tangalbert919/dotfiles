4af8db426bed84f74b445c3fa342406f50d13bcb
Albert Tang <tangalbert919@yahoo.com>
2022-02-10 15:24:04 -0600

    Switch to kitty terminal
    I find kitty more useful than Alacritty.

--- main/user-variables.lua
+++ main/user-variables.lua
@@ -7,7 +7,7 @@ local home = os.getenv("HOME")
 local _M = {
   -- This is used later as the default terminal and editor to run.
   -- terminal = "xterm",
-  terminal = "alacritty",
+  terminal = "kitty",
    
   -- Default modkey.
   -- Usually, Mod4 is the key with a logo between Control and Alt.
